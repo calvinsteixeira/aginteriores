c6f1721e25a5b87c24d8bcf5269583ed7efc53d6
Calvin <calvingsx@gmail.com>
2024-09-11 08:34:00 -0300

    Posicionando a imagem na primeira seção

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <div className="">
       <main className="bg-background">        
-        <section></section>
+        <section className="h-[30rem] bg-primary -mx-6">
+          <Image
+            src={'/imgbg1.png'}
+            alt="Imagem de um ambiente com paredes claras, 2 sofás brancos e uma mesa de apoio com potes de decoração."
+            objectFit="cover"
+            fill
+          />
+        </section>
       </main>
     </div>
   );
